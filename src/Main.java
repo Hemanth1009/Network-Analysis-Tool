14ef37e390f8ba0a8f2d2c23f689f21a8c25b88d
Hemanthraj V <gdronzer03@gmail.com>
2025-08-07 11:50:04 +0530

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -5,7 +5,6 @@ public class Main
     public static void main(String[] args) {
         DBConnector.connect();
         DBConnector.createTable();
-//        DBConnector.insertSignal("WiFi Router",-68);
         DBConnector.displaySignal();
         SwingUtilities.invokeLater(DBConnector.SignalLoggerUI::new);
     }
